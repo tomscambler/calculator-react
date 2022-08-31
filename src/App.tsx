ddec511c5134930a5c42e290a738c56a549e0392
tomscambler <tomscambler@gmail.com>
2022-08-31 19:05:36 +0100

    calculator html working

---
+++ src/App.tsx
@@ -0,0 +1,137 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import './Calculator/Calculator.css'
+
+
+function evaluateCalculation(display:number, operation:string, operand:number|null):number{
+
+  if(operand === null){
+    return display;
+  }
+  if (operand === 0 && operation === '/'){
+    alert("BEHAVE!");
+    return 0;
+  }
+  if (operand === 0 && operation === '^' && display === 0){
+    alert("BEHAVE!");
+    return 0;
+  }
+  switch(operation){
+
+    case '+':
+      return display + operand;
+      break;
+    case '-':
+      return display - operand;
+      break;
+    case '*':
+      return display * operand;
+      break;
+    case '/':
+      return display / operand;
+      break;
+    case '^':
+      return Math.pow(display,operand);
+      break;
+    case '=':
+      return display;
+      break;
+    default:
+      return operand;
+  }
+}
+
+function App() {
+
+  const [display,   setDisplay  ] = useState<number>     (0 );
+  const [operand,   setOperand  ] = useState<number|null>(null);
+  const [operation, setOperation] = useState<string>     ('');
+
+  const numberSymbols    = [1,2,3,4,5,6,7,8,9,0];
+  const operationSymbols = ['+','-','*','/','^'];
+  const powerSymbols     = ['=','C','AC','del'];
+
+  return (
+    <div className="calculator">
+
+      <div className="displayViewer">
+        {display}
+      </div>
+
+      <div className="operationViewer">
+        {operation}
+      </div>
+
+      <div className="operandViewer">
+        {operand}
+      </div>
+
+      <div>
+        {numberSymbols.map( numberSymbol => (
+            <button className="numberButton" onClick={ () => {
+              setOperand( (operand===null?0:operand)*10 + numberSymbol )
+              }}>
+                {numberSymbol}
+            </button>
+            )
+        )}
+      </div>
+
+      <div>
+        {operationSymbols.map( operationSymbol => (
+
+            <button className="operationButton" onClick={ () => {
+                setDisplay(evaluateCalculation(display,operation,operand));
+                setOperation(operationSymbol);
+                setOperand(null);
+              }
+            }>
+
+              {operationSymbol}
+
+            </button>
+          )
+        )}
+      </div>
+
+      <div>
+        {powerSymbols.map( powerSymbol => (
+
+            <button className="powerButton" onClick={ () => {
+                switch(powerSymbol){
+                  case  '=':
+                    setDisplay(evaluateCalculation(display,operation,operand));
+                    setOperation('=');
+                    setOperand(null);
+                    break;
+                  case 'AC':
+                    setOperand(null);
+                    setDisplay(0);
+                    setOperation('');
+                    break
+                  case  'C':
+                    setOperand(display);
+                    setDisplay(0);
+                    setOperation('');
+                    break;
+                  case  'del':
+                    setOperand(operand===null?null:parseFloat(operand.toString().slice(0,-1)));
+                    break;
+                }
+              }
+            }>
+
+              {powerSymbol}
+
+            </button>
+          )
+        )}
+      </div>
+      
+    </div>
+
+  );
+}
+
+export default App
---
+++ src/Calculator/Button.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function Button({ButtonSymbol}: {ButtonSymbol:string}) {
+    return (                
+        <button>
+            {ButtonSymbol}
+        </button>
+    );
+}
